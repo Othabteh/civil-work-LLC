9d5e5ceea64f4e2ecae321df08a76c3201925dd8
Othabteh <o.thabteh@gmail.com>
2020-06-23 23:24:34 +0300

    add

--- script/script.js
+++ script/script.js
@@ -7,30 +7,29 @@ var greeting;
 var greeting2;
 var greeting3;
 
-var yourage = prompt('please enter your age!');
+var yourage = prompt("please enter your age!");
 if (yourage > 18) {
-    greeting = 'Welcome to our website!';
+    greeting = "Welcome to our website!";
 } else {
-    greeting = 'you are not allowd to open this site!';
+    greeting = "you are not allowd to open this site!";
 }
 
-var yourmajor = prompt('please enter your major!');
-if (yourmajor = 'engineering') {
+var yourmajor = prompt("please enter your major!");
+if (yourmajor = "engineering") {
 
-    greeting2 = 'welcome to the engineers heaven!';
+    greeting2 = "together we will make difference!";
 }
 else {
-    greeting2 = 'this website is only for engineers!';
+    greeting2 = "this website is only for engineers!";
 }
-var contact = prompt('Are you intrested to cotact us!');
-if (contact = 'yes') {
-    greeting3 = 'you are more than welcom to contact us!';
+var contact = prompt("Are you intrested to cotact us!");
+if (contact = "yes") {
+    greeting3 = "you are more than welcom to contact us!";
 
 }
 else {
-    greeting3 = 'thank you for visit our website';
+    greeting3 = "thank you for visit our website";
 }
-
 
 
 document.write('<h3>' + greeting + greeting2 + greeting3 + '</h3>');
